91e96de36778dd0327dc4684951acf039eb67adb
ameliendrez <ameliendrez@gmail.com>
2018-04-20 18:21:55 -0300

    Correcion tp1

---
+++ TP1/MySimpleLinkedList.java
@@ -0,0 +1,191 @@
+package practico1;
+
+import java.util.Iterator;
+
+public class MySimpleLinkedList implements Iterable<Object> {
+	
+	private Node first;
+	private Node last;
+	private int nodeCount;
+	private Node cursor;
+	
+	
+	public class MyIterator implements Iterator<Object> {
+
+		private Node cursor;
+		
+		@Override
+		public boolean hasNext() {
+			return cursor != null;
+		}
+
+		@Override
+		public Object next() {
+			Node tmp = cursor;
+			cursor = cursor.getNext();
+			return tmp.getInfo();
+		}
+		
+		public void remove(){
+			//Método para implementar	
+		}
+		
+		private MyIterator (Node first){
+			this.cursor = first;
+		}
+	}
+
+	public MySimpleLinkedList() {
+		this.first = null;
+		this.last = null;
+		this.nodeCount = 0;
+	}
+	public void insert(Object o) {
+		Node tmp = new Node(o, null);
+		tmp.setNext(first);
+		this.first = tmp;
+		if (last == null) {
+			last = first;
+		}
+		nodeCount++;
+	}
+	
+	public void insertLast(Object o) {
+		if((search((int)o) == null )) {
+			Node tmp = new Node(o,null);
+			if (last!=null) {
+				last.setNext(tmp);
+				last = tmp;
+			}else {
+				last = tmp;
+				first = tmp;
+			}
+			this.nodeCount++;
+		}	
+	}
+
+	public Object extract() {
+		if (!this.isEmpty()) {
+			Node retorno = first;
+			this.first = first.getNext();
+			this.nodeCount--;
+			return retorno;
+		}
+		return null;
+	}
+
+	public boolean isEmpty() {
+		return first == null;
+	}
+	public int size() { 
+		return this.nodeCount;
+	}
+	
+	public void resetCursor(){
+		this.cursor = first;
+	}
+	
+	public Object get(){
+		Object element = cursor.getInfo();
+		this.cursor = cursor.getNext();
+		return element;
+	}
+	
+	public boolean cursorHasNext(){
+		return cursor != null;
+	}
+	
+	@Override
+	public MyIterator iterator() {
+		return new MyIterator(first);
+	}
+	
+	public void insertOrganized(Object elemento) {
+		
+		if (this.isEmpty() || (int)first.getInfo() > (int)elemento) {
+			this.insert(elemento);
+		}
+		else {
+			Node cursor = first;
+			while (cursor.getNext() != null  && cursor.getInfo() != elemento && cursor.getNext().getInfo() != elemento) {
+				if ((int)cursor.getNext().getInfo() > (int)elemento) {
+					Node tmp = new Node(elemento, cursor.getNext());
+					cursor.setNext(tmp);
+					nodeCount++;
+					break;
+				}
+				cursor = cursor.getNext();
+			}
+			if ((cursor.getNext() == null && cursor.getInfo()!=elemento)) {
+				this.insertLast(elemento);
+			}
+		}
+	}
+	
+	public MySimpleLinkedList combinarListasDesordenas(MySimpleLinkedList lista) {
+
+		MySimpleLinkedList newList = new MySimpleLinkedList();		
+		MyIterator elemL1 = this.iterator();
+
+		while (elemL1.hasNext()) {
+			int valorE1 = (int)elemL1.next();
+			MyIterator elemL2 = lista.iterator();
+
+			while (elemL2.hasNext()) {	
+				int valorE2 = (int)elemL2.next();
+				if (valorE1 == valorE2) {
+					newList.insertOrganized(valorE1);
+					break;
+				}
+			}
+		}
+		return newList;
+	}
+	
+	public MySimpleLinkedList combinarListasOrdenadas (MySimpleLinkedList lista) {
+		
+		MySimpleLinkedList newList = new MySimpleLinkedList();		
+		MyIterator elemL1 = this.iterator();
+
+		while (elemL1.hasNext()) {
+			int valorE1 = (int)elemL1.next();
+			MyIterator elemL2 = lista.iterator();
+
+			while (elemL2.hasNext()) {	
+				int valorE2 = (int)elemL2.next();
+				if (valorE1 == valorE2) {
+					newList.insertLast(valorE1);
+					break;
+				}
+				else if (valorE1 < valorE2) {
+					break;
+				}
+			}
+		}
+		return newList;
+	}
+	
+	public Node getFirst() {
+		return first;	
+	}	
+	
+
+	public void printElement(int n) {
+		System.out.println(this.search(n).getInfo());;
+	}
+
+	public Node search(int n) {
+		if (n < this.nodeCount) {
+
+			Node actual = first;
+			int iterador = 0;
+
+			while (iterador < n) {
+				actual = actual.getNext();
+				iterador++;
+			}
+			return actual;
+		}
+		return null;
+	}
+}
---
+++ TP1/PracticoMain.java
@@ -0,0 +1,122 @@
+package practico1;
+import practico1.MySimpleLinkedList.MyIterator;
+
+public class PracticoMain {
+
+	public static void main(String[] args) {
+		
+		//Creo las Listas Ordenadas 1 y 2
+		
+		MySimpleLinkedList listaOrdenada1 = new MySimpleLinkedList();
+		MySimpleLinkedList listaOrdenada2 = new MySimpleLinkedList();
+
+		for (int i = 0; i < 15; i++){
+			
+			// La lista ordenada 1 es de 0 a 15. 
+			listaOrdenada1.insertLast(i);
+			
+			// La lista ordenada 2 son los divisibles por dos entre el 0 y el 15
+			
+			if ((i%2) == 0) {
+				listaOrdenada2.insertLast(i);
+			}
+		}
+		
+		System.out.println ("Imprimo la lista ordenada 1:");
+
+		//Creo iterador de la Lista Ordenada 1 e imprimo
+		
+		MyIterator iteradorLO1 = listaOrdenada1.iterator();
+
+		while (iteradorLO1.hasNext()) {
+			System.out.print (iteradorLO1.next() + " - ");
+		}
+		System.out.println ("");
+		System.out.println ("");
+		System.out.println ("Imprimo la lista ordenada 2:");
+		
+		//Creo iterador de la Lista Ordenada 2 e imprimo
+
+		MyIterator iteradorLO2 = listaOrdenada2.iterator();
+
+		while (iteradorLO2.hasNext()) {
+			System.out.print (iteradorLO2.next() + " - ");
+		}
+		System.out.println ("");
+
+		
+		//Creo la Lista ordenada 3
+		
+		MySimpleLinkedList listaOrdenada3 = new MySimpleLinkedList();
+		
+		// Combino las listas ordenadas 1 y 2
+		listaOrdenada3 = listaOrdenada1.combinarListasOrdenadas(listaOrdenada2);
+		
+		System.out.println ("");
+		System.out.println ("Imprimo la lista ordenada 3:");
+
+		//Creo iterador de la Lista Ordenada 3 e imprimo
+
+		MyIterator iteradorLO3 = listaOrdenada3.iterator();
+
+		while (iteradorLO3.hasNext()) {
+			System.out.print (iteradorLO3.next() + " - ");
+		}
+		
+
+		//Creo las listas desordenadas 1 y 2
+		
+		MySimpleLinkedList listaDesordenada1 = new MySimpleLinkedList();
+		MySimpleLinkedList listaDesordenada2 = new MySimpleLinkedList();
+
+		
+		for (int i = 0; i < 10; i++){
+			
+			// Completo las listas desordenadas con valores entre el 1 y el 25 
+			listaDesordenada1.insertLast((int)(Math.random() * 25) + 1);
+			listaDesordenada2.insertLast((int)(Math.random() * 25) + 1);
+		}
+
+		System.out.println ("");
+		System.out.println ("");
+		System.out.println ("Imprimo la lista desordenada 1:");
+
+		//Creo iterador de la Lista Desordenada 1 e imprimo
+
+		MyIterator iteradorLD1 = listaDesordenada1.iterator();
+
+		while (iteradorLD1.hasNext()) {
+			System.out.print (iteradorLD1.next() + " - ");
+		}
+		
+		System.out.println ("");
+		System.out.println ("");
+		System.out.println ("Imprimo la lista desordenada 2:");
+
+		//Creo iterador de la Lista Desordenada 2 e imprimo
+
+		MyIterator iteradorLD2 = listaDesordenada2.iterator();
+
+		while (iteradorLD2.hasNext()) {
+			System.out.print (iteradorLD2.next() + " - ");
+		}
+		
+		//Creo la lista desordenada 3
+		MySimpleLinkedList listaDesordenada3 = new MySimpleLinkedList();
+		
+		//Combino las listas desordenadas 1 y 2
+		listaDesordenada3 = listaDesordenada1.combinarListasDesordenas(listaDesordenada2);
+		
+		System.out.println ("");
+		System.out.println ("");
+		System.out.println ("Imprimo la lista desordenada 3:");
+
+		//Creo iterador de la Lista Desordenada 3 e imprimo
+
+		MyIterator iteradorLD3 = listaDesordenada3.iterator();
+
+		while (iteradorLD3.hasNext()) {
+			System.out.print (iteradorLD3.next() + " - ");
+		}
+	}
+}
